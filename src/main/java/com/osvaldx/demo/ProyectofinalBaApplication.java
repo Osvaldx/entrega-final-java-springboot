7da7c7c3bfead75151417c88f5123dcd752a9edc
Osvaldx <romanonahuel24@gmail.com>
2025-11-20 19:51:18 -0300

    [+] proyecto base

---
+++ src/main/java/com/osvaldx/demo/ProyectofinalBaApplication.java
@@ -0,0 +1,13 @@
+package com.osvaldx.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectofinalBaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectofinalBaApplication.class, args);
+	}
+
+}
