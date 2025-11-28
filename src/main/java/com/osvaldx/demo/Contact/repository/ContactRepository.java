4b82a95dd2062d0b20e5fa85b51ea0f178bc4e82
Osvaldx <romanonahuel24@gmail.com>
2025-11-27 21:27:16 -0300

    [+] creamos el repositorio de contacto con metodos personalizados

--- src/main/java/com/osvaldx/demo/Contact/repository/ContactRepository.java
+++ src/main/java/com/osvaldx/demo/Contact/repository/ContactRepository.java
@@ -1,4 +1,20 @@
 package com.osvaldx.demo.Contact.repository;
 
-public interface ContactRepository {
+import com.osvaldx.demo.Contact.enums.ContactCategory;
+import com.osvaldx.demo.Contact.model.Contact;
+// Importamos JpaRepository, que nos da los métodos CRUD listos
+import org.springframework.data.jpa.repository.JpaRepository;
+// Importamos la anotación @Repository que marca esta interfaz como componente de acceso a datos
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+    List<Contact> findContactsByCategory(ContactCategory category);
+    List<Contact> findByPhone(String phone);
+    List<Contact> findByEmail(String email);
+    List<Contact> findByFirstnameContaining(String text);
+
 }
