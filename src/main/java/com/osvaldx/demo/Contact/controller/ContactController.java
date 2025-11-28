3552f1f220a8902f7fd50781526774e365bf5dbd
Osvaldx <romanonahuel24@gmail.com>
2025-11-28 03:23:18 -0300

    Desarrollamos los demas endpoints que faltaban y su mini logica

--- src/main/java/com/osvaldx/demo/Contact/controller/ContactController.java
+++ src/main/java/com/osvaldx/demo/Contact/controller/ContactController.java
@@ -1,12 +1,11 @@
 package com.osvaldx.demo.Contact.controller;
 
+import com.osvaldx.demo.Contact.enums.ContactCategory;
 import com.osvaldx.demo.Contact.model.Contact;
 import com.osvaldx.demo.Contact.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +24,50 @@ public class ContactController {
     @GetMapping
     public List<Contact> findAll() {
         return this.contactService.findAllContacts();
+    }
+
+    @GetMapping(params = "phone")
+    public ResponseEntity<Contact> findContactByPhone(@RequestParam String phone) {
+        return this.contactService.findContactByPhone(phone)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(params = "email")
+    public ResponseEntity<Contact> findContactByEmail(@RequestParam String email) {
+        return this.contactService.findContactByEmail(email)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(params = "category")
+    public List<Contact> findContactsByCategory(@RequestParam ContactCategory category) {
+        return this.contactService.findContactsByCategory(category);
+    }
+
+    @PostMapping
+    public ResponseEntity<Contact> saveContact(@RequestBody Contact contact) {
+        Contact contactCreated = this.contactService.createContact(contact);
+        return ResponseEntity.ok(contactCreated);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Contact> updateContact(@PathVariable Long id, @RequestBody Contact contact) {
+        return this.contactService.findContactById(id)
+                .map(contactFound -> {
+                    Contact contactUpdated = this.contactService.updateContact(id, contact);
+                    return ResponseEntity.ok(contactUpdated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteContact(@PathVariable Long id) {
+        if (this.contactService.findContactById(id).isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        this.contactService.deleteContact(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
