077b5aca25a7ab47519b099f51c7e1afc9e2f1e9
Osvaldx <romanonahuel24@gmail.com>
2025-11-28 03:21:55 -0300

    Cambiamos el tipo de retorno de los metodos por un Optional.

--- src/main/java/com/osvaldx/demo/Contact/service/ContactService.java
+++ src/main/java/com/osvaldx/demo/Contact/service/ContactService.java
@@ -4,15 +4,16 @@ import com.osvaldx.demo.Contact.enums.ContactCategory;
 import com.osvaldx.demo.Contact.model.Contact;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ContactService {
 
     Contact createContact(Contact contact);
     List<Contact> findAllContacts();
+    Optional<Contact> findContactById(Long id);
     List<Contact> findContactsByCategory(ContactCategory category);
-    List<Contact> findContactByPhone(String phone);
-    List<Contact> findContactByEmail(String email);
-    List<Contact> findContactByFistnameContaining(String text);
+    Optional<Contact> findContactByPhone(String phone);
+    Optional<Contact> findContactByEmail(String email);
     Contact updateContact(Long id, Contact contact);
     void deleteContact(Long id);
 
