9a62fa09d311791dfa53213cc235d783a5c828f4
Osvaldx <romanonahuel24@gmail.com>
2025-11-27 21:27:56 -0300

    [+] implementamos la interfaz con sus metodos y le hacemos override y utilizamos el repositorio

--- src/main/java/com/osvaldx/demo/Contact/service/ContactServiceImpl.java
+++ src/main/java/com/osvaldx/demo/Contact/service/ContactServiceImpl.java
@@ -1,4 +1,59 @@
 package com.osvaldx.demo.Contact.service;
 
-public class ContactServiceImpl {
+import com.osvaldx.demo.Contact.enums.ContactCategory;
+import com.osvaldx.demo.Contact.model.Contact;
+import com.osvaldx.demo.Contact.repository.ContactRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContactServiceImpl implements ContactService{
+
+    private final ContactRepository contactRepository;
+
+    public ContactServiceImpl(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    @Override
+    public Contact createContact(Contact contact) {
+        return contactRepository.save(contact);
+    }
+
+    @Override
+    public List<Contact> findAllContacts() {
+        return contactRepository.findAll();
+    }
+
+    @Override
+    public List<Contact> findContactsByCategory(ContactCategory category) {
+        return contactRepository.findContactsByCategory(category);
+    }
+
+    @Override
+    public List<Contact> findContactByPhone(String phone) {
+        return contactRepository.findByPhone(phone);
+    }
+
+    @Override
+    public List<Contact> findContactByEmail(String email) {
+        return contactRepository.findByEmail(email);
+    }
+
+    @Override
+    public List<Contact> findContactByFistnameContaining(String text) {
+        return contactRepository.findByFirstnameContaining(text);
+    }
+
+    @Override
+    public Contact updateContact(Long id ,Contact contact) {
+        contact.setId(id);
+        return contactRepository.save(contact);
+    }
+
+    @Override
+    public void deleteContact(Long id) {
+        contactRepository.deleteById(id);
+    }
 }
