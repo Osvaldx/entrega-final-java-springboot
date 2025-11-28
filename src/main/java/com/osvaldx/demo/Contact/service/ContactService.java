c43f02efafc4903f32610d8e184347ee4ae10c2e
Osvaldx <romanonahuel24@gmail.com>
2025-11-27 21:26:50 -0300

    [+] Creamos el servicio con todos los metodos que vamos a usar en los endpoints

--- src/main/java/com/osvaldx/demo/Contact/service/ContactService.java
+++ src/main/java/com/osvaldx/demo/Contact/service/ContactService.java
@@ -1,4 +1,19 @@
 package com.osvaldx.demo.Contact.service;
 
+import com.osvaldx.demo.Contact.enums.ContactCategory;
+import com.osvaldx.demo.Contact.model.Contact;
+
+import java.util.List;
+
 public interface ContactService {
+
+    Contact createContact(Contact contact);
+    List<Contact> findAllContacts();
+    List<Contact> findContactsByCategory(ContactCategory category);
+    List<Contact> findContactByPhone(String phone);
+    List<Contact> findContactByEmail(String email);
+    List<Contact> findContactByFistnameContaining(String text);
+    Contact updateContact(Long id, Contact contact);
+    void deleteContact(Long id);
+
 }
